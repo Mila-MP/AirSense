e10b5064ae47b1edb37ae8bad92e318fcd6977f3
zc2020-imperial <zc2020@ic.ac.uk>
2022-12-11 18:07:33 +0000

    data_storage - added ability to add inhalers to inhaler database Still unable to add data specific to each inhaler

--- src/main/java/Inhaler.java
+++ src/main/java/Inhaler.java
@@ -1,2 +1,50 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Inhaler {
+
+    public String inhaler_expiry;
+    public String inhaler_name;
+    public int puffs_left;
+
+    public Inhaler(String inhaler_type, String expiry, int quantity) {
+        this.inhaler_name = inhaler_type;
+        this.inhaler_expiry = expiry;
+        this.puffs_left = quantity;
+
+
+    }
+
+    public void add_inhaler(){
+        String dbUrl = "jdbc:postgresql://localhost:5432/postgres";
+
+        try {
+            // Registering the driver
+            Class.forName("org.postgresql.Driver");
+            System.out.println("Connecting to the database");
+            Connection conn = DriverManager.getConnection(dbUrl, "postgres", "airsense");
+            System.out.println("You are now connected to the database");
+
+            System.out.println("Inserting data into inhaler");
+            Statement s = conn.createStatement();
+            String sqlStr = "INSERT INTO inhalers (inhaler_type,expiry_date,quantity) VALUES ('reliver2','0202',5)";
+            System.out.println(sqlStr);
+
+            s.executeUpdate(sqlStr);
+
+            sqlStr = "INSERT INTO inhalers (inhaler_type,expiry_date,quantity) VALUES ('reliver2','0202',5);";
+            s.executeUpdate(sqlStr);
+// This is not working so sad
+            conn.commit();
+            s.close();
+            conn.close();
+        } catch (Exception e) {
+        }
+
+    }
+
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,25 @@
 import javax.swing.*;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException{
-        JFrame frame= new JFrame("AirSense");
-        frame.setSize(500,500);
+    public static void main(String[] args) throws IOException, SQLException {
+        JFrame frame = new JFrame("AirSense");
+        frame.setSize(500, 500);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         PollutionInfo page = new PollutionInfo();
         frame.add(page.getMainPanel());
+
+        Inhaler myreliever = new Inhaler("reliver", "12/12/12", 200);
+        myreliever.add_inhaler();
+
+
     }
 
 }
