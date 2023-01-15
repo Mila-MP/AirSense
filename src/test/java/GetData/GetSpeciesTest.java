c91ca869a7fff115d89d47eecdcc6d9db83d3d00
Mila-MP <mdp20@ic.ac.uk>
2023-01-15 01:16:54 +0000

    Final touches

--- src/test/java/GetData/GetSpeciesTest.java
+++ src/test/java/GetData/GetSpeciesTest.java
@@ -9,15 +9,12 @@ class GetSpeciesTest {
     @Test
     void print() throws IOException {
         GetSpecies species = new GetSpecies();
-        assertEquals(
-                """
-                        CO: Carbon Monoxide
-                        NO2: Nitrogen Dioxide
-                        O3: Ozone
-                        PM10: PM10 Particulate
-                        PM25: PM2.5 Particulate
-                        SO2: Sulphur Dioxide
-                        """,
+        assertEquals("CO: Carbon Monoxide\n" +
+                        "NO2: Nitrogen Dioxide\n" +
+                        "O3: Ozone\n" +
+                        "PM10: PM10 Particulate\n" +
+                        "PM25: PM2.5 Particulate\n" +
+                        "SO2: Sulphur Dioxide\n",
                         species.print());
     }
 }
